0a7c0e68f5c01a8c61fa13e55b72f128f768c71d
PeterDauwe <bobo5290461@gmail.com>
2022-02-25 08:31:57 +0100

    added aries-wallpaper1

--- aries-wallpaper1/build-aries.sh
+++ aries-wallpaper1/build-aries.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
-deletion1="../../noobie-repo/x86_64/"
+deletion1="../../aries_repo/x86_64/"
 name1="${PWD##*/}"
 
 rm $deletion1/$name1-21*.zst
 
 
 
-destiny="../../noobie-repo/x86_64/"
+destiny="../../aries_repo/x86_64/"
 
 #destiny=$destination1
 
@@ -17,7 +17,7 @@ echo "Moving created files to " $destiny
 echo "#############################################################################################"
 mv $search*pkg.tar.zst $destiny
 
-ls | grep -v "PKGBUILD\|build-noobie.sh" | xargs rm -rf
+ls | grep -v "PKGBUILD\|build-aries.sh" | xargs rm -rf
 
 echo "Cleaning up"
 echo "#############################################################################################"
